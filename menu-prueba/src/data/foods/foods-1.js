1fdf2233feb3a6c7ac6fd81d420f6a83e769be7f
nicodsd <nilonb7@gmail.com>
2025-10-13 17:19:11 -0300

    carpetas, data, arreglos

---
+++ menu-prueba/src/data/foods/foods-1.js
@@ -0,0 +1,39 @@
+const foods = [
+    {
+        id: 1,
+        name: "Pizza Margherita",
+        description: "Clásica pizza italiana con tomate, mozzarella y albahaca.",
+        price: 8.99,
+        image: "/images/pizza-margherita.jpg"
+    },
+    {
+        id: 2,
+        name: "Hamburguesa",
+        description: "Jugosa hamburguesa de res con queso, lechuga y tomate.",
+        price: 7.5,
+        image: "/images/hamburguesa.jpg"
+    },
+    {
+        id: 3,
+        name: "Ensalada César",
+        description: "Ensalada fresca con pollo, crutones y aderezo César.",
+        price: 6.0,
+        image: "/images/ensalada-cesar.jpg"
+    },
+    {
+        id: 4,
+        name: "Sushi Roll",
+        description: "Rollos de sushi rellenos de salmón y aguacate.",
+        price: 9.5,
+        image: "/images/sushi-roll.jpg"
+    },
+    {
+        id: 5,
+        name: "Tacos al Pastor",
+        description: "Tacos mexicanos de cerdo marinado con piña y cebolla.",
+        price: 5.75,
+        image: "/images/tacos-al-pastor.jpg"
+    }
+];
+
+export default foods;
